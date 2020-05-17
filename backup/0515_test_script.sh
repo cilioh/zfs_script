ef8df428362711a8c7a699c8a63dbf4d0f21b704
Chungyong KIM <chungyongkim@gmail.com>
2020-05-18 06:51:22 +0900

    200518-0651

--- backup/0515_test_script.sh
+++ backup/0515_test_script.sh
@@ -26,7 +26,7 @@ echo "" > /mnt/share/cykim/result/gc_throughputCN8.txt
 echo "" > /mnt/share/cykim/result/gc_throughputCN9.txt
 echo "" > /mnt/share/cykim/result/gc_throughputCN10.txt
 #Loop
-for stripe in 8 16 32 64 128
+for stripe in 48
 do
 	echo "ON" > ${sig_dir}/breaksig
 	sleep 1
