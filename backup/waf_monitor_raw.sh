01f320f8f10cb233b18257239b40160c0edab1ab
Chungyong KIM <chungyongkim@gmail.com>
2020-05-18 20:44:24 +0900

    200518-2044

--- backup/waf_monitor_raw.sh
+++ backup/waf_monitor_raw.sh
@@ -51,7 +51,7 @@ do
 	        WAF_1=$(echo "$nand_writes_bf_1 $user_writes_bf_1 $nand_writes_at_1 $user_writes_at_1" | awk '{printf "%.4f \n", ($3-$1)/($4-$2)}')
 	fi
 
-	echo $WAF_1 >> /mnt/share/cykim/result/pm_waf${1}.txt
+	echo $WAF_0 $WAF_1 >> /mnt/share/cykim/result/pm_waf${1}.txt
 done
 
 exit 0
