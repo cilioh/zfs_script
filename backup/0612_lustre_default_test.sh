25d479a58d00c5cf18679fffb283e738322a5785
Chungyong KIM <chungyongkim@gmail.com>
2020-06-13 15:09:58 +0900

    200613-1509

--- backup/0612_lustre_default_test.sh
+++ backup/0612_lustre_default_test.sh
@@ -10,15 +10,19 @@ todaytime=`date "+%H%M"`
 mkdir -p /mnt/share/cykim/result/${todaydate}
 echo ${todaydate}"-"${todaytime} > /mnt/share/cykim/result/${todaydate}/Result_${todaytime}_${nodename}.txt
 
-for bsize in "4G" "8G" "16G" "32G"
+#for bsize in "4G" "8G" "16G" "32G"
+for bsize in "4G"
 do
-	for numjobs in "1" "2" "4" "8" "16"
+	for numjobs in "4"
+#	for numjobs in "1" "2" "4" "8" "16"
 	do
-		for stripecount in "1" "2" "4" "8" "16"
+		for stripecount in "8"
+#		for stripecount in "1" "2" "4" "8" "16"
 		do
 			lfs setstripe -c ${stripecount} /mnt/lustre
 
-			for iter in {1..5}
+			for iter in {1..2}
+#			for iter in {1..5}
 			do
 				rm -rf /mnt/lustre/*
 				sleep 2
--- backup/fio_script.sh
+++ backup/fio_script.sh
@@ -12,7 +12,7 @@ iternum=${8}
 directory="/mnt/lustre"
 sig_dir="/mnt/share/cykim/signal"
 
-	output=`fio --directory=${directory} --name=${filename}${iternum} --rw=write --direct=0 --bs=1M --size=${bsize} --numjobs=${numjobs} --group_reporting --fallocate=none | awk '$1 == "WRITE:" { print $3 } /9[5-9].[0-9]+th/ { print }' | grep -oP '(\([0-9.]+|95.00th=\[[\s\d]+|99.00th=\[[\s\d]+|99.90th=\[[\s\d]+|99.99th=\[[\s\d]+)' | grep -oP '[\(\[]\s*(\d+^[.]|\d+.\d+)' | grep -oP '(\d+^[.]|\d+.\d+)' | tr '\n' ' ' | awk '{ print $1"," $2"," $3"," $4"," $5 }'`
+	output=`fio --directory=${directory} --name=${filename}${iternum} --rw=write --direct=0 --bs=1M --size=${bsize} --numjobs=${numjobs} --group_reporting --fallocate=none | awk '$1 == "WRITE:" { print $3 } $1 == "iops" { print } /9[5-9].[0-9]+th/ { print }' | grep -oP '(\([0-9.]+|95.00th=\[[\s\d]+|99.00th=\[[\s\d]+|99.90th=\[[\s\d]+|99.99th=\[[\s\d]+|min=[\s\d.]+|max=[\s\d.]+|avg=[\s\d.]+)' | grep -oP '([\(\[]\s*(\d+^[.]|\d+.\d+)|=\s*(\d+^[.]|\d+.\d+))' | grep -oP '(\d+^[.]|\d+.\d+)' | tr '\n' ' ' | awk '{ print $1"," $2"," $3"," $4"," $5"," $6"," $7"," $8}'`
 
 	#Result - ECHO
 	echo ${nodename}","${bsize}","${numjobs}","${stripecount}","${iternum}","${output} >> /mnt/share/cykim/result/${todaydate}/Result_${todaytime}_${nodename}.txt
@@ -21,8 +21,11 @@ sig_dir="/mnt/share/cykim/signal"
 	per99=`echo $output | cut -d',' -f2`
 	per999=`echo $output | cut -d',' -f3`
 	per9999=`echo $output | cut -d',' -f4`
-	th=`echo $output | cut -d',' -f5`
-	echo "["${nodename}",BJ="${bsize}",NJ="${numjobs}",SC="${stripecount}",ITER-"${iternum}"],95%=["${per95}"],99%=["${per99}"],99.9%=["${per999}"],99.99%=["${per9999}"],Throughput=["${th}"]"
+	iopsmin=`echo $output | cut -d',' -f5`
+	iopsmax=`echo $output | cut -d',' -f6`
+	iopsavg=`echo $output | cut -d',' -f7`
+	th=`echo $output | cut -d',' -f8`
+	echo "["${nodename}",BJ="${bsize}",NJ="${numjobs}",SC="${stripecount}",IT-"${iternum}"],99%=["${per99}"],99.99%=["${per9999}"],iops_avg=["${iopsavg}"],TH=["${th}"]"
 	sleep 1
 
 exit 0
