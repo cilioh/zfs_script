600d9c200b9902edc629614925dbb89218a7634c
Chungyong KIM <chungyongkim@gmail.com>
2020-06-26 21:59:19 +0900

    200626-2159

--- backup/fio_script.sh
+++ backup/fio_script.sh
@@ -9,10 +9,11 @@ todaydate=${6}
 todaytime=${7}
 iternum=${8}
 
-directory="/mnt/lustre"
+directory="/mnt/nvme"
 sig_dir="/mnt/share/cykim/signal"
+direct=1  #0: buffered , 1:directIO
 
-	output=`fio --directory=${directory} --name=${filename}${iternum} --rw=write --direct=0 --bs=1M --size=${bsize} --numjobs=${numjobs} --group_reporting --fallocate=none | awk '$1 == "WRITE:" { print $3 } $1 == "iops" { print } /9[5-9].[0-9]+th/ { print }' | grep -oP '(\([0-9.]+|95.00th=\[[\s\d]+|99.00th=\[[\s\d]+|99.90th=\[[\s\d]+|99.99th=\[[\s\d]+|min=[\s\d.]+|max=[\s\d.]+|avg=[\s\d.]+)' | grep -oP '([\(\[]\s*(\d+^[.]|\d+.\d+)|=\s*(\d+^[.]|\d+.\d+))' | grep -oP '(\d+^[.]|\d+.\d+)' | tr '\n' ' ' | awk '{ print $1"," $2"," $3"," $4"," $5"," $6"," $7"," $8}'`
+	output=`fio --directory=${directory} --name=${filename}${iternum} --rw=write --direct=${direct} --bs=1M --size=${bsize} --numjobs=${numjobs} --group_reporting --fallocate=none | awk '$1 == "WRITE:" { print $3 } $1 == "iops" { print } /9[5-9].[0-9]+th/ { print }' | grep -oP '(\([0-9.]+|95.00th=\[[\s\d]+|99.00th=\[[\s\d]+|99.90th=\[[\s\d]+|99.99th=\[[\s\d]+|min=[\s\d.]+|max=[\s\d.]+|avg=[\s\d.]+)' | grep -oP '([\(\[]\s*(\d+^[.]|\d+.\d+)|=\s*(\d+^[.]|\d+.\d+))' | grep -oP '(\d+^[.]|\d+.\d+)' | tr '\n' ' ' | awk '{ print $1"," $2"," $3"," $4"," $5"," $6"," $7"," $8}'`
 
 #	ssh pm1 'kill -9 $(pidof iostat)'
 #	ssh pm2 'kill -9 $(pidof iostat)'
