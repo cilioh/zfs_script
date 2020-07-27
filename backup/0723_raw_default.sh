9f0e24ef0684e107c026d41a54998c1cfd398395
Chungyong Kim <chungyongkim@gmail.com>
2020-07-27 21:05:25 +0900

    200727-2105

--- backup/0723_raw_default.sh
+++ backup/0723_raw_default.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-directory="/mnt/nvme"
+directory="/nvme"
 sig_dir="/mnt/share/cykim/signal"
 nodename="PM1"
 filename="apple"
@@ -17,9 +17,9 @@ do
 	do
 		for bsize in "4G" "8G" "16G" "32G"
 		do
-			for numjobs in "1" "2" "4" "8" "16"
+			for numjobs in "1" "2" "4" "8"
 			do
-				for iter in {1..5}
+				for iter in {1..1}
 				do
 					rm -rf /mnt/nvme/*
 					sleep 5
@@ -30,8 +30,8 @@ do
 
 					/mnt/share/cykim/backup/result_iostat_save.sh ${todaydate} ${todaytime} ${nodename} 4
 
-					totval=0
-					totval=`df -h | awk '$1=="/dev/md0" { print $5 }' | grep -oP '\d+'`
+#					totval=`df -h | awk '$1=="/dev/nvme0n1" { print $5 }' | grep -oP '\d+'`
+					totval=`df -h | awk '$1=="nvme" { print $5 }' | grep -oP '\d+'`
 					if [ $totval -ge 98 ]; then
 						echo " Storage 98% FULL --- EMPTY "
 						rm -rf ${directory}/${filename}*
@@ -44,6 +44,6 @@ do
 done
 
 duration=$SECONDS
-echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed. SCRIPT DONE"
+echo "$(($duration / 3600)) hours, $(($duration % 3600 / 60)) minutes, and $(($duration % 60)) seconds elapsed. SCRIPT DONE"
 
 exit 0
