0e55eea767627d5a13256282c3eb9a0ff57ca2fc
Chungyong KIM <chungyongkim@gmail.com>
2020-09-17 20:46:18 +0900

    200917-2046

--- backup/lustre-zfs-start.sh
+++ backup/lustre-zfs-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mdsname="mds2"
+mdsname="mds2e"
 
 echo "mdt / oss1 / cn"
 
@@ -39,13 +39,13 @@ fi
 
 if [[ $1 == "oss1" ]]; then
 
-	mkfs.lustre --ost --backfstype=zfs --index=1 --reformat --mgsnode=${mdsname}i@o2ib --fsname=lustre ost0/ost0 /dev/nvme0n1
+	mkfs.lustre --ost --backfstype=zfs --index=1 --reformat --mgsnode=${mdsname}@o2ib --fsname=lustre ost0/ost0 /dev/nvme0n1
 	sleep 2
-	mkfs.lustre --ost --backfstype=zfs --index=2 --reformat --mgsnode=${mdsname}i@o2ib --fsname=lustre ost1/ost1 /dev/nvme1n1
+	mkfs.lustre --ost --backfstype=zfs --index=2 --reformat --mgsnode=${mdsname}@o2ib --fsname=lustre ost1/ost1 /dev/nvme1n1
 	sleep 2
-	mkfs.lustre --ost --backfstype=zfs --index=3 --reformat --mgsnode=${mdsname}i@o2ib --fsname=lustre ost2/ost2 /dev/nvme2n1
+	mkfs.lustre --ost --backfstype=zfs --index=3 --reformat --mgsnode=${mdsname}@o2ib --fsname=lustre ost2/ost2 /dev/nvme2n1
 	sleep 2
-	mkfs.lustre --ost --backfstype=zfs --index=4 --reformat --mgsnode=${mdsname}i@o2ib --fsname=lustre ost3/ost3 /dev/nvme3n1
+	mkfs.lustre --ost --backfstype=zfs --index=4 --reformat --mgsnode=${mdsname}@o2ib --fsname=lustre ost3/ost3 /dev/nvme3n1
 	sleep 2
 #fi
 
@@ -116,7 +116,7 @@ fi
 
 if [[ $1 == "cn" ]]; then
 
-	mount -t lustre ${mdsname}i@o2ib:/lustre /mnt/lustre
+	mount -t lustre ${mdsname}@o2ib:/lustre /mnt/lustre
 	sleep 3
 	chmod 777 /mnt/lustre
 	sleep 2
